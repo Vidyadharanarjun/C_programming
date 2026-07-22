862354d5f4614ff1c437047c75e9f1c11f89349d
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-07-22 11:16:54 -0700

    add integer.c

---
+++ C_practice/integer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    
+    // Prompt and read integer input
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    
+    // Print the stored integer
+    printf("You entered: %d\n", number);
+    
+    return 0;
+}   
