f8caa19b9f582c06e0d8fd084f9e62543a2d780c
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-07-22 12:06:40 -0700

    add arithmetic

---
+++ C_practice/arithmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int a = 10, b = 3;
+printf("Sum : %d, \n",a + b); 
+printf("diff : %d \n", a - b);
+printf("product : %d \n", a * b);
+printf("quotient : %d, \n", a / b);
+return 0;
+}
+
+
+
+
