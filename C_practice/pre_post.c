85bf0c920cfb8ba8005389369d9a904f4372556e
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-07-23 09:47:23 -0700

    add prepost

---
+++ C_practice/pre_post.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ()
+{
+int a = 10;
+printf("Initial a = %d\n\n", a);
+
+printf("output of a++ : %d\n", a++);
+printf("value of a now : %d\n", a);
+
+printf("output of ++a : %d\n", ++a);
+printf("value of a : %d\n", a);
+
+printf("output of a-- : %d\n", --a);
+printf("value of a : %d\n", a);
+
+printf("output of --a : %d\n", --a);
+printf("output of a : %d\n", a);
+return 0;
+}
+
